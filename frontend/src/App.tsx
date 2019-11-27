26fbc0f9b762b84564ed30aa85d32c31de8f12de
Igor Czajkowski <igor.czajkowski.93@gmail.com>
2019-11-27 20:58:44 +0100

    Add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import axios from "axios";
+import Routes from "./Routes";
 
 const App: React.FC = () => {
-  const [message, setMessage] = useState("");
-
-  axios
-    .get("/api/users/all")
-    .then(response => setMessage(response.data.message));
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {message}
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes />;
 };
 
 export default App;
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LoginPage: React.FC = () => <div>This is login page</div>;
+
+export default LoginPage;
