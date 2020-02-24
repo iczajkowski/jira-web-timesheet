a7f344d872c260bdb0b1b1f5a8bbc5e08d272cc8
Igor Czajkowski <igor.czajkowski.93@gmail.com>
2020-02-24 22:21:33 +0100

    Add deployment script

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+param (
+    [Parameter(Mandatory=$true)][string]$version
+)
+
+git checkout -b release/v.$version
+cd .\frontend\
+Remove-Item .\build\ -Confirm:$false -Recurse -ErrorAction Ignore
+npm run build
+cd ..\backend\
+Remove-Item .\dist\ -Confirm:$false -Recurse -ErrorAction Ignore
+npm run build
+git add .\dist\
+git commit -m "Release v.$version" --no-verify
+git push origin
+eb deploy jira-timesheet-prod
