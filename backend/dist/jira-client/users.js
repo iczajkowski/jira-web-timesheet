6410dbf1bc4cbd3001c46d37481c11a6a2a134bc
Igor Czajkowski <igor.czajkowski.93@gmail.com>
2020-06-26 07:51:43 +0200

    Release v.2.4.0

---
+++ backend/dist/jira-client/users.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findUsers = ({ query, jiraClient }) => jiraClient.user.search({ query });
+exports.getUser = ({ accountId, jiraClient }) => {
+    const options = {
+        uri: jiraClient.buildURL("/user"),
+        method: "GET",
+        json: true,
+        followAllRedirects: true,
+        qs: { accountId }
+    };
+    return jiraClient.makeRequest(options);
+};
