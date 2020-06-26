bcf91183e672d40f867ed6eb20fe0b16ed111b0d
Igor Czajkowski <igor.czajkowski.93@gmail.com>
2020-06-26 09:10:58 +0200

    Release v.2.4.2

---
+++ backend/dist/jira-client/configuration-validator.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const isEmail_1 = __importDefault(require("validator/lib/isEmail"));
+const isURL = require("validator/lib/isURL");
+exports.cofigValidator = (clientConfig) => {
+    return (clientConfig &&
+        validateEmail(clientConfig) &&
+        validateURL(clientConfig) &&
+        clientConfig.apiToken);
+};
+const validateEmail = ({ email }) => {
+    return email && isEmail_1.default(email);
+};
+const validateURL = ({ url }) => {
+    return (url && isURL(url) && (!url.startsWith("http") || url.startsWith("https")));
+};
---
+++ backend/dist/public/precache-manifest.2ae29702798797b96d99f9d6cacd8916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ef546e946564a235a5c34d0f592aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2ad7c2e6fdc023c80f",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "7204f7e1dc8c2e4d5ecd",
+    "url": "/static/css/main.20e20a77.chunk.css"
+  },
+  {
+    "revision": "1e2ad7c2e6fdc023c80f",
+    "url": "/static/js/2.c7ac4b42.chunk.js"
+  },
+  {
+    "revision": "7204f7e1dc8c2e4d5ecd",
+    "url": "/static/js/main.b42b9c81.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
