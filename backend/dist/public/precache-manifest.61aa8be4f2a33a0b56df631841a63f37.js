bfbf725fd6dd55b9b531d6e37492d312737efa13
Igor Czajkowski <igor.czajkowski.93@gmail.com>
2020-11-15 20:12:45 +0100

    Release v.2020-11-15

---
+++ backend/dist/public/precache-manifest.61aa8be4f2a33a0b56df631841a63f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91af1434729e1abf7315e96d0326afdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c87b91d2172ec0231a2a",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "25b284a549527126d596",
+    "url": "/static/css/main.1e07e3f0.chunk.css"
+  },
+  {
+    "revision": "c87b91d2172ec0231a2a",
+    "url": "/static/js/2.4a6c8594.chunk.js"
+  },
+  {
+    "revision": "25b284a549527126d596",
+    "url": "/static/js/main.cda1980d.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
